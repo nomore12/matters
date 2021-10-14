43520671ca6f8891aad42b04cfea8d54b20a476d
sungho noh <nightwing@naver.com>
2021-10-14 10:50:04 +0900

    admin install

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(5000, () => console.log('backend test'));
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { Landing } from './pages/index';
-import { CssBaseline, CSSBaseLine } from '@mui/material';
+import { CssBaseline } from '@mui/material';
 import styled from 'styled-components';
 import './App.css';
+import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
+
+
+
+
 
 const AppContainer = styled.div`
   height: 100%;
@@ -14,7 +19,16 @@ function App() {
     <AppContainer className="App">
       <CssBaseline />
       <Router>
-        <Landing />
+        <Switch>
+          <Route exact path="/">
+            <Landing />
+          </Route>
+          <Route path="/admin">
+            {/* <Admin dataProvider={dataProvider} >
+              <Resource name="uses" list={UserList} edit={EditGuesser} />
+            </Admin> */}
+          </Route>
+        </Switch>
       </Router>
     </AppContainer>
   );

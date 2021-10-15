4fb699e292bdc14e62b79ddb9ebdd9376487e88b
sungho noh <nightwing@naver.com>
2021-10-15 14:36:12 +0900

    admin

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,38 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+const whiteList = ["*"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not Allowed Origin'));
+    }
+  }
+}
+
+const images = {
+  "images": [
+    { "title": "Irure", "id": 1, "desc": "Irure magna minim enim consectetur ad cupidatat.", "date": "2021-10-13" },
+    { "title": "Amet", "id": 2, "desc": "Amet veniam eiusmod qui culpa nulla.", "date": "2021-10-14" },
+    { "title": "Lorem", "id": 3, "desc": "Lorem id quis ut elit aliquip in deserunt.", "date": "2021-10-14" },
+    { "title": "Exercitation", "id": 4, "desc": "Exercitation veniam ea veniam occaecat veniam.", "date": "2021-10-15" }
+  ]
+};
+
+app.use(cors());
+
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.send('hello world');
+})
+
+app.get('/api/images', (req, res) => {
+  res.json({ok: true, images: images});
 })
 
 app.listen(5000, () => console.log('backend test'));
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { Landing } from './pages/index';
 import { CssBaseline } from '@mui/material';
 import styled from 'styled-components';
 import './App.css';
 import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
+import fadeDataProvider from 'ra-data-fakerest';
+import { ImageCreate, ImageEdit, ImageList } from './admin/images';
+import jsonServerProvider from 'ra-data-simple-rest';
+import simpleRestProvider from 'ra-data-simple-rest';
 
-
-
-
+// https://jsonplaceholder.typicode.com/
 
 const AppContainer = styled.div`
   height: 100%;
 `;
 
+// const provider = 
+
 function App() {
+
   return (
     <AppContainer className="App">
       <CssBaseline />
@@ -24,9 +29,9 @@ function App() {
             <Landing />
           </Route>
           <Route path="/admin">
-            {/* <Admin dataProvider={dataProvider} >
-              <Resource name="uses" list={UserList} edit={EditGuesser} />
-            </Admin> */}
+            <Admin dataProvider={simpleRestProvider('http://localhost:5000/api')} >
+              <Resource name="images" list={ImageList} create={ImageCreate} edit={ImageEdit} />
+            </Admin>
           </Route>
         </Switch>
       </Router>

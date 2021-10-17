b906cbf5de22d9b6232bc53e5d7458cc16687994
sungho noh <nightwing@naver.com>
2021-10-17 19:57:12 +0900

    redux setting

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -8,27 +8,19 @@ function LoginForm(props) {
   
   const signIn = () => signInWithPopup(auth, provider)
   .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
     const credential = GoogleAuthProvider.credentialFromResult(result);
     const token = credential.accessToken;
     console.log('credential', credential, token);
-    // The signed-in user info.
     const user = result.user;
     console.log('user', user);
-    // ...
   }).catch((error) => {
-    // Handle Errors here.
     const errorCode = error.code;
     console.log(errorCode);
     const errorMessage = error.message;
     console.log(errorMessage);
-    // The email of the user's account used.
     const email = error.email;
     console.log(email);
-    // The AuthCredential type that was used.
     const credential = GoogleAuthProvider.credentialFromError(error);
-    console.log(credential);
-    // ...
   });
 
   const login = async (e) => {
---
+++ frontend/src/feature/userSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    loggedIn: false,
+    reducer: {
+      login: (state) => {
+        state.loggedIn = true;
+      },
+      logout: (state) => {
+        state.loggedIn = false;
+      }
+    }
+  }
+})
+
+export const { login, logout } = userSlice.reducer;
+export default userSlice.reducer;

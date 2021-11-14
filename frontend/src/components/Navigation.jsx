a3b6fbeb7a9155893f080539c40761c48896dfdc
Sungho Noh <nightwing@naver.com>
2021-11-14 13:33:45 +0900

    layout

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NaveWrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+
+  & > button {
+    /* margin-left: 40px; */
+  }
+`;
 
 function Navigation() {
   return (
-    <h1>nav</h1>
+    <NaveWrapper>
+      <button>ABOUT</button>
+      <button>PROJECT</button>
+      <button>CONTACT</button>
+      <button>MATTERS</button>
+    </NaveWrapper>
   )
 }
 
--- frontend/src/containers/Main.jsx
+++ frontend/src/containers/Main.jsx
@@ -1,43 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import {ReactComponent as Logo} from 'static/images/logo.svg';
+import { ReactComponent as Logo } from 'static/images/logo.svg';
+import { Navigation } from 'components';
 
 const Container = styled.div`
   display: flex;
   width: 100%;
   height: 100vh;
-  justify-content: center;
-  align-items: flex-start;
-  /* background-color: #eee; */
-  padding: 0px 84px;
+  flex-direction: column;
+  background-color: aqua;
+  padding: 0px 120px;
 `;
 
-const ContentArea = styled.div`
+const Row = styled.div`
   display: flex;
-  width: 100%;
-  /* background-color: #fff; */
-  height: 100%;
-  flex-wrap: wrap;
   justify-content: space-between;
-  align-items: flex-start;
-  align-content: flex-start;
 `;
 
 const Block = styled.div`
   width: ${(props) => (props.width)};
   height: ${(props) => (props.height)};
   border: 1px solid black;
+  padding-top: ${props => props.paddingTop};
   /* https://db2dev.tistory.com/entry/React-resize-이벤트-다루기 */
   /* display: ${props => (props.width < 20 ? 'none' : 'flex')}; */
 `;
 
 const LogoWrapper = styled.div`
   display: flex;
-  align-items: flex-end;
+  /* align-items: flex-end;
+  justify-content: flex-start; */
 `;
 
 const LogoComp = styled(Logo)`
   height: ${props => props.height};
+  width: 123px;
 
   @media only screen and (max-width: 768px) {
     height: 72px;
@@ -49,21 +46,24 @@ const CONTENT_WIDTH = 640;
 const HEADER_HEIGHT = 136;
 
 function Main() {
+  const firstRowStyles = {display: 'flex', alignItems: 'flex-end'}
+
   return (
     <>
       <Container>
-        <ContentArea>
-          <Block width={`${ASIDE_WIDTH}px`} height={`${HEADER_HEIGHT}px`}>
-            <LogoWrapper>
+        <Row>
+          <Block width={`${ASIDE_WIDTH}px`} height={`${HEADER_HEIGHT}px`} style={firstRowStyles}>
+            <LogoWrapper >
               <LogoComp height={`${86}px`}></LogoComp>
             </LogoWrapper>
           </Block>
-          <Block width={`calc(100% - ${ASIDE_WIDTH}px - ${CONTENT_WIDTH}px - 2px)`} height={`${HEADER_HEIGHT}px`}>2</Block>
-          <Block width={`${CONTENT_WIDTH}px`} height={`${HEADER_HEIGHT}px`}>3</Block>
-          <Block width={`${ASIDE_WIDTH}px`} height={ `calc(100vh - ${HEADER_HEIGHT}px)`}>1</Block>
-          <Block width={`calc(100% - ${ASIDE_WIDTH}px - ${CONTENT_WIDTH}px - 2px)`} height={ `calc(100vh - ${HEADER_HEIGHT}px)`}>2</Block>
-          <Block width={`${CONTENT_WIDTH}px`} height={ `calc(100vh - ${HEADER_HEIGHT}px)`}>3</Block>
-        </ContentArea>
+          {/* navigation */}
+          <Block width={`${CONTENT_WIDTH}px`} height={`${HEADER_HEIGHT}px`} style={firstRowStyles}><Navigation /></Block>
+        </Row>
+        <Row>
+          <Block paddingTop={"40px"} width={`${ASIDE_WIDTH}px`} height={ `calc(100vh - ${HEADER_HEIGHT}px)`}>menu</Block>
+          <Block paddingTop={"40px"} width={`${CONTENT_WIDTH}px`} height={ `calc(100vh - ${HEADER_HEIGHT}px)`}>content</Block>
+        </Row>
       </Container>
     </>
   )
